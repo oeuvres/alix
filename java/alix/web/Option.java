90c1ba788d253190e82787791fd9f9c89a745921
Frédéric Glorieux <frederic.glorieux@fictif.org>
2021-01-19 01:47:00 +0100

    Bug for filtered options

--- java/alix/web/Option.java
+++ java/alix/web/Option.java
@@ -1,3 +1,35 @@
+/*
+ * Alix, A Lucene Indexer for XML documents.
+ * 
+ * Copyright 2009 Pierre Dittgen <pierre@dittgen.org> 
+ *                Frédéric Glorieux <frederic.glorieux@fictif.org>
+ * Copyright 2016 Frédéric Glorieux <frederic.glorieux@fictif.org>
+ *
+ * Alix is a java library to index and search XML text documents
+ * with Lucene https://lucene.apache.org/core/
+ * including linguistic expertness for French,
+ * available under Apache license.
+ * 
+ * Alix has been started in 2009 under the javacrim project
+ * https://sf.net/projects/javacrim/
+ * for a java course at Inalco  http://www.er-tim.fr/
+ * Alix continues the concepts of SDX under another licence
+ * «Système de Documentation XML»
+ * 2000-2010  Ministère de la culture et de la communication (France), AJLSM.
+ * http://savannah.nongnu.org/projects/sdx/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package alix.web;
 
 import java.lang.reflect.Field;
@@ -43,12 +75,13 @@ public interface Option
     StringBuilder sb = new StringBuilder();
     // let cry if list is empty
     String[] values = list.split("\\s+");
-    Class cls = this.getClass();
+    Class cls = ((Enum<?>) this).getDeclaringClass();
     for (String name: values) {
       Option opt = null;
       try {
         opt = (Option) Enum.valueOf(cls, name);
       } catch (IllegalArgumentException e) {
+        sb.append("<!-- " + name +"  " + cls + "  " + e + "\n");
         continue;
       }
       html(sb, opt);
