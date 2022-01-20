f87c53f843e91473d2699872cf9dbc930d8a1cc0
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-01-20 23:37:49 +0100

    renaming to Order

--- java/alix/web/OptionOrder.java
+++ java/alix/web/OptionOrder.java
@@ -1,26 +1,26 @@
 package alix.web;
 
-import alix.lucene.search.FormEnum.Sorter;
+import alix.lucene.search.FormEnum.Order;
 
 public enum OptionOrder implements Option 
 {
-    score("Score", null, Sorter.score),
-    freq("Occurrences", null, Sorter.freq),
-    hits("Textes", null, Sorter.hits),
-    occs("Total occurrences", null, Sorter.occs),
-    docs("Total textes", null, Sorter.docs),
-    alpha("Alphabétique", null, Sorter.alpha),
+    score("Score", null, Order.score),
+    freq("Occurrences", null, Order.freq),
+    hits("Textes", null, Order.hits),
+    occs("Total occurrences", null, Order.occs),
+    docs("Total textes", null, Order.docs),
+    alpha("Alphabétique", null, Order.alpha),
     ;
-    private OptionOrder(final String label, final String hint, Sorter sorter) {    
+    private OptionOrder(final String label, final String hint, Order order) {    
         this.label = label;
         this.hint = hint;
-        this.sorter = sorter;
+        this.order = order;
     }
-    final public Sorter sorter;
+    final public Order order;
     final public String label;
     final public String hint;
     public String label() { return label; }
     public String hint() { return hint; }
-    public Sorter sorter() { return sorter; }
+    public Order order() { return order; }
 
 }
