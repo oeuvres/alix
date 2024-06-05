3e46aa4392fc8ea9ffd992e7188271f0eaad0109
Frédéric Glorieux <frederic.glorieux@fictif.org>
2024-06-05 20:33:56 +0200

    Save test

---
+++ src/test/java/com/github/oeuvres/alix/lucene/analysis/FlagFindFilterTest.java
@@ -0,0 +1,49 @@
+package com.github.oeuvres.alix.lucene.analysis;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
+import org.apache.lucene.analysis.tokenattributes.PositionLengthAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+import org.junit.Test;
+
+public class FlagFindFilterTest {
+
+    @Test
+    public void testPos() throws IOException
+    {
+        Analyzer analyzer = new FrAnalyzer();
+        analyze(analyzer, "test", "Janet est un psychologue.");
+    }
+    
+    public void analyze(Analyzer analyzer, String field, String text) throws IOException
+    {
+        TokenStream tokenStream = analyzer.tokenStream(field, text);
+        
+
+        final CharTermAttribute termAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+        final OffsetAttribute offsetAttribute = tokenStream.addAttribute(OffsetAttribute.class);
+        final TypeAttribute typeAttribute = tokenStream.addAttribute(TypeAttribute.class);
+        final PositionIncrementAttribute posIncAttribute =
+            tokenStream.addAttribute(PositionIncrementAttribute.class);
+        final PositionLengthAttribute posLenAttribute =
+            tokenStream.addAttribute(PositionLengthAttribute.class);
+        
+        tokenStream.reset();
+        while(tokenStream.incrementToken()) {
+            System.out.print(
+                termAttribute.toString() + "\t" 
+              + offsetAttribute.startOffset() + "\t" 
+              + offsetAttribute.endOffset() + "\t" 
+              + posIncAttribute.getPositionIncrement() + "\n"
+            );
+        }
+    }
+
+}
--- src/test/java/com/github/oeuvres/alix/util/DirTest.java
+++ src/test/java/com/github/oeuvres/alix/util/DirTest.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.Test;
 
