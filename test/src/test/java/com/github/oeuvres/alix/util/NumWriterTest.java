16ce752e5e812b98cec38d524021d9a87c83bda3
Frédéric Glorieux <frederic.glorieux@fictif.org>
2026-03-18 13:03:28 +0100

    Rename

--- test/src/test/java/com/github/oeuvres/alix/util/NumWriterTest.java
+++ test/src/test/java/com/github/oeuvres/alix/util/NumWriterTest.java
@@ -16,7 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
-public class IntWriterTest
+public class NumWriterTest
 {
     @TempDir
     Path tempDir;
